a6ba49bff1b8b82118edf00e6432bdc9ccb89244
Jeffrey Silverman <jsilverman@gmail.com>
2017-08-13 21:08:31 -0700

    Made the script more resilient against DNS problems, also made the naming of envars more consistent

--- monitor.sh
+++ monitor.sh
@@ -2,8 +2,9 @@
 
 TEST_HOST="www.commercialventvac.com"
 TEST_HOST_IP="208.97.189.29"
-DEFAULT_ROUTER="192.168.0.1"		# This assumes the default router is also a name server, true for home networks, false for businesses
+DEFAULT_ROUTER_IP="192.168.0.1"		# This assumes the default router is also a name server, true for home networks, false for businesses
 ISPS_ROUTER="tukw-dsl-gw67.tukw.qwest.net"
+ISPS_ROUTER_IP="63.231.10.67"
 
 
 # For more information about putting colors in scripts, see for example
@@ -13,43 +14,43 @@ echo -e -n "${WHITE}"
 date
 echo -e -n "${NC}"
 exitstatus=0
-if ping -c 4 $DEFAULT_ROUTER  > /dev/null 2>/dev/null; then
-  echo -e  "${GREEN_BG}Path to router is UP${NC}"
+if ping -c 4 $DEFAULT_ROUTER_IP  > /dev/null 2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to LAN router is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to router is DOWN${NC}"
+  echo -e  "${RED_BG}ping to LAN router is DOWN${NC}"
   exitstatus=1
 fi
-if ping -c 4 $ISPS_ROUTER > /dev/null  2>/dev/null; then
-  echo -e  "${GREEN_BG}Path to ISP's router ${ISPS_ROUTER} is UP${NC}"
+if ping -c 4 $ISPS_ROUTER_IP > /dev/null  2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to ISP's router ${ISPS_ROUTER_IP} is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to ISP's router ${ISPS_ROUTER} is DOWN${NC}"
+  echo -e  "${RED_BG}ping to ISP's router ${ISPS_ROUTER_IP} is DOWN${NC}"
   exitstatus=1
 fi
 if ping -c 4 $TEST_HOST_IP > /dev/null  2>/dev/null; then
-  echo -e  "${GREEN_BG}Path to ${TEST_HOST} without using DNS is UP${NC}"
+  echo -e  "${GREEN_BG}ping to ${TEST_HOST} without using DNS is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to ${TEST_HOST} without using DNS is DOWN${NC}"
+  echo -e  "${RED_BG}ping to ${TEST_HOST} without using DNS is DOWN${NC}"
   exitstatus=1
 fi
 if ping -c 4 $TEST_HOST > /dev/null  2>/dev/null; then
-  echo -e  "${GREEN_BG}Path to ${TEST_HOST} using DNS is UP${NC}"
+  echo -e  "${GREEN_BG}ping to ${TEST_HOST} using DNS is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to ${TEST_HOST} using DNS is DOWN${NC}"
+  echo -e  "${RED_BG}ping to ${TEST_HOST} using DNS is DOWN${NC}"
   exitstatus=1
 fi
 if ping -c 4 8.8.8.8 > /dev/null  2>/dev/null; then
-  echo -e  "${GREEN_BG}Path to Google's name server  is UP${NC}"
+  echo -e  "${GREEN_BG}ping to Google's name server  is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to Google's name server  is DOWN${NC}"
+  echo -e  "${RED_BG}ping to Google's name server  is DOWN${NC}"
   exitstatus=1
 fi
-if dig @$DEFAULT_ROUTER desktop > /dev/null ; then
+if dig @${DEFAULT_ROUTER_IP} desktop > /dev/null ; then
   echo -e  "${GREEN_BG}DNS on router to desktop (tests internal configuration) is UP${NC}"
 else
-  echo -e  "${RED_BG}DNS on router is DOWN${NC}"
+  echo -e  "${RED_BG}DNS on router to desktop is DOWN${NC}"
   exitstatus=1
 fi
-if dig @$DEFAULT_ROUTER $TEST_HOST > /dev/null ; then
+if dig @${DEFAULT_ROUTER_IP} $TEST_HOST > /dev/null ; then
   echo -e  "${GREEN_BG}DNS on router to $TEST_HOST (tests external confguration) is UP${NC}"
 else
   echo -e  "${RED_BG}DNS on router to $TEST_HOST is DOWN${NC}"
