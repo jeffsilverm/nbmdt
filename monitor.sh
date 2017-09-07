71d39605f7f2f9a81bfb94c74239989d2db1c6c0
Jeff Silverman <jeffsilverm@gmail.com>
2017-09-07 13:17:02 -0700

    Latest updates.  monitor.sh has support for testing DNS resolution of jeffsilverm.ddns.net and jeffsilverman.ddns.net

--- monitor.sh
+++ monitor.sh
@@ -1,96 +1,126 @@
 #! /bin/bash
 
-TEST_HOST="f5.com"
-TEST_HOST_IP="104.219.111.168"
-DEFAULT_ROUTER="192.168.0.1"		# This assumes the default router is also a name server, true for home networks, false for businesses
+TEST_HOST="www.commercialventvac.com"
+TEST_HOST_IP="208.97.189.29"
+DEFAULT_ROUTER_IP="192.168.0.1"		# This assumes the default router is also a name server, true for home networks, false for businesses
+ISPS_ROUTER="tukw-dsl-gw67.tukw.qwest.net"
+ISPS_ROUTER_IP="63.231.10.67"
 
-# This script creates colors for any bash script that invokes it
-BLACK='\033[0;30m'
-DARKGRAY='\033[1;30m'
-RED='\033[0;31m'
-LIGHTRED='\033[1;31m'
-GREEN='\033[0;32m'
-LIGHTGREEN='\033[1;32m'
-BROWN='\033[0;33m' 
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-LIGHTBLUE='\033[1;34m'
-PURPLE='\033[0;35m'
-LIGHTPURPLE='\033[1;35m'
-CYAN='\033[0;36m'
-LIGHTCYAN='\033[1;36m'
-LIGHTGRAY='\033[0;37m'     
-WHITE='\033[1;37m'
-NC='\033[0m'
-# This script creates colors for any bash script that invokes it
-BLACK_BG='\033[0;49m'
-DARKGRAY_BG='\033[1;40m'
-RED_BG='\033[0;41m'
-LIGHTRED_BG='\033[1;41m'
-GREEN_BG='\033[0;42m'
-LIGHTGREEN_BG='\033[1;42m'
-BROWN_BG='\033[0;43m' 
-YELLOW_BG='\033[1;43m'
-BLUE_BG='\033[0;44m'
-LIGHTBLUE_BG='\033[1;44m'
-PURPLE_BG='\033[0;45m'
-LIGHTPURPLE_BG='\033[1;45m'
-CYAN_BG='\033[0;46m'
-LIGHTCYAN_BG='\033[1;46m'
-LIGHTGRAY_BG='\033[0;47m'     
-WHITE_BG='\033[1;47m'
-echo -e -n "${PURPLE}"
-date
-echo -e -n "${NC}"
+
+# For more information about putting colors in scripts, see for example
+# http://misc.flogisoft.com/bash/tip_colors_and_formatting
+source ./colors.sh
+echo -e "\033[1m\033[37;40m$(date)\033[0m"
 exitstatus=0
-if ping -c 4 $DEFAULT_ROUTER  > /dev/null; then
-  echo -e  "${GREEN_BG}Path to router is UP${NC}"
+if ping -c 4 $DEFAULT_ROUTER_IP  > /dev/null 2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to LAN router is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to router is DOWN${NC}"
+  echo -e  "${RED_BG}ping to LAN router is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "The local router is not pingable"
 fi
-if ping -c 4 $TEST_HOST_IP > /dev/null ; then
-  echo -e  "${GREEN_BG}Path to F5.com without using DNS is UP${NC}"
+if ping -c 4 $ISPS_ROUTER_IP > /dev/null  2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to ISP's router ${ISPS_ROUTER_IP} is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to F5.com without using DNS is DOWN${NC}"
+  echo -e  "${RED_BG}ping to ISP's router ${ISPS_ROUTER_IP} is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "The ISP's router is not pingable"
 fi
-if ping -c 4 $TEST_HOST > /dev/null ; then
-  echo -e  "${GREEN_BG}Path to F5.com using DNS is UP${NC}"
+if ping -c 4 $TEST_HOST_IP > /dev/null  2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to ${TEST_HOST} without using DNS is UP${NC}"
 else
-  echo -e  "${RED_BG}Path to F5.com using DNS is DOWN${NC}"
+  echo -e  "${RED_BG}ping to ${TEST_HOST} without using DNS is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "The test host $TEST_HOST_IP is not pingable"
 fi
-if dig @$DEFAULT_ROUTER desktop > /dev/null ; then
+if ping -c 4 $TEST_HOST > /dev/null  2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to ${TEST_HOST} using DNS is UP${NC}"
+else
+  echo -e  "${RED_BG}ping to ${TEST_HOST} using DNS is DOWN${NC}"
+  exitstatus=1
+  spd-say -w  "The test host $TEST_HOST is not pingable"
+fi
+if ping -c 4 8.8.8.8 > /dev/null  2>/dev/null; then
+  echo -e  "${GREEN_BG}ping to Google's name server  is UP${NC}"
+else
+  echo -e  "${RED_BG}ping to Google's name server  is DOWN${NC}"
+  exitstatus=1
+  spd-say -w  "Google's name server is not pingable"
+fi
+if dig @${DEFAULT_ROUTER_IP} desktop > /dev/null ; then
   echo -e  "${GREEN_BG}DNS on router to desktop (tests internal configuration) is UP${NC}"
 else
-  echo -e  "${RED_BG} on router is DOWN${NC}"
+  echo -e  "${RED_BG}DNS on router to desktop is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "The name server on the local router fails to answer DNS queries for desktop"
 fi
-if dig @$DEFAULT_ROUTER $TEST_HOST > /dev/null ; then
+if dig @${DEFAULT_ROUTER_IP} $TEST_HOST > /dev/null ; then
   echo -e  "${GREEN_BG}DNS on router to $TEST_HOST (tests external confguration) is UP${NC}"
 else
-  echo -e  "${RED_BG}DNS on router to $TEST_HOST nis DOWN${NC}"
+  echo -e  "${RED_BG}DNS on router to $TEST_HOST is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "The name server on the local router fails to answer DNS queries for $TEST_HOST "
 fi
 if dig @8.8.8.8 $TEST_HOST > /dev/null ; then
-  echo -e  "${GREEN_BG}DNS on Google's name server to F5.com is UP${NC}"
+  echo -e  "${GREEN_BG}DNS on Google's name server to ${TEST_HOST} is UP${NC}"
 else
-  echo -e  "${RED_BG}DNS on Google's name server to F5.com is DOWN${NC}"
+  echo -e  "${RED_BG}DNS on Google's name server to ${TEST_HOST} is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "Google's name server fails to answer queries for ${TEST_HOST} "
 fi
 if dig $TEST_HOST | fgrep $TEST_HOST_IP > /dev/null ; then
-  echo -e  "${GREEN_BG}DNS on the default name server to F5.com is UP${NC}"
+  echo -e  "${GREEN_BG}DNS on the default name server to ${TEST_HOST} is UP${NC}"
 else
-  echo -e  "${RED_BG}DNS on the default name server to F5.com is DOWN${NC}"
+  echo -e  "${RED_BG}DNS on the default name server to ${TEST_HOST} is DOWN${NC}"
   exitstatus=1
+  spd-say -w  "Default name server fails to answer queries for ${TEST_HOST} "
+fi
+if nc -z ${TEST_HOST} 22 > /dev/null ; then
+  echo -e  "${GREEN_BG}SSH on ${TEST_HOST} is UP${NC}"
+else
+  echo -e  "${RED_BG}SSH on ${TEST_HOST} is ${RED_BG}DOWN${NC}"
+  exitstatus=1
+  spd-say -w  "SSH on ${TEST_HOST} fails"
 fi
 if curl -s http://$TEST_HOST > /dev/null ; then
-  echo -e  "${GREEN_BG}HTTP on F5.com is UP${NC}"
+  echo -e  "${GREEN_BG}HTTP on ${TEST_HOST} is UP${NC}"
 else
-  echo -e  "${RED_BG}HTTP on F5.com is ${RED_BG}DOWN${NC}"
+  echo -e  "${RED_BG}HTTP on ${TEST_HOST} is ${RED_BG}DOWN${NC}"
   exitstatus=1
+  spd-say -w  "HTTP on ${TEST_HOST} fails"
 fi
-exit $exitstatus
+
+if ping -c 4 jeffsilverman.ddns.net > /dev/null; then
+  echo -e "${GREEN_BG}jeffsilverman.ddns.net DNS lookup is UP${NC}"
+elif [ $? -eq 2 ] ; then
+  echo -e "${RED_BG}jeffsilverman.ddns.net DNS lookup is not resolving${NC}"
+  exitstatus=1
+elif [ $? -eq 1 ]; then
+  echo -e "${YELLOW_BG}jeffsilverman.ddns.net DNS lookup is resolving but the external address is not pingable${NC}"
+  exitstatus=1
+  if [[ "$(host jeffsilverman.ddns.net)" == *75.172.110.119 ]]; then
+    echo -e "${YELLOW_BG}jeffsilverman.ddns.net DNS lookup is resolving to ${GREEN_BG}75.172.110.119${NC}, but it is  ${RED_BG}not pingable${NC}" 
+  else
+    echo -e "${YELLOW_BG}jeffsilverman.ddns.net DNS lookup is resolving to ${RED_BG}$(host jeffsilverman.ddns.net)${YELLOW_BG}, not ${GREEN_BG}75.172.110.119${NC}"
+  fi 
+fi
+
+if ping -c 4 jeffsilverm.ddns.net > /dev/null; then
+  echo -e "${GREEN_BG}jeffsilverm.ddns.net DNS lookup is UP${NC}"
+elif [ $? -eq 2 ] ; then
+  echo -e "${RED_BG}jeffsilverm.ddns.net DNS lookup is not resolving${NC}"
+  exitstatus=1
+elif [ $? -eq 1 ]; then
+  host_ipv4=`host jeffsilverm.ddns.net|fgrep -v IPv6`
+  echo -e "${YELLOW_BG}jeffsilverm.ddns.net DNS lookup is resolving but the external address ${host_ipv4} is not pingable${NC}"
+  exitstatus=1
+  if [[ "$()" == *75.172.110.119 ]]; then
+    echo -e "${YELLOW_BG}jeffsilverm.ddns.net DNS lookup is resolving to ${GREEN_BG}75.172.110.119${NC}, but it is  ${RED_BG}not pingable${NC}" 
+  else
+    echo -e "${YELLOW_BG}${host_ipv4}, not ${GREEN_BG}75.172.110.119${NC}"
+  fi 
+fi
+
+exit 
 
 
