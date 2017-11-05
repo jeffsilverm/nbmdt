cb2a563bd6e62b28b5e4ff5c90423f4e66025d6b
Jeff Silverman <jeffsilverm@gmail.com>
2017-11-04 17:36:18 -0700

    Fixed up the nbmdt.ini file
    Signed-off-by: Jeff Silverman <jeffsilverm@gmail.com>

--- configuration.py
+++ configuration.py
@@ -24,16 +24,18 @@ class FixedConfiguration(object):
         """
 
         self.__ini_filename = ini_filename
-        config = configparser.ConfigParser()
-        if len ( config.read(ini_filename) ) == 0:
+        self.config = configparser.ConfigParser()
+        if len ( self.config.read(ini_filename) ) == 0:
             raise FileNotFoundError("%s was not found" % ini_filename )
-        sections = config.sections()
-        self.hostname = os.environ["HOSTNAME"]
-        if self.hostname not in sections:
-            raise ValueError("hostname %s is not a section in the configuration file %s" % (self.hostname),
-                             ini_filename )
-        my_section = config[self.hostname]
-        self.__ip_command = my_section['ip_command']
+        if False:
+            sections = self.config.sections()
+            self.hostname = os.environ["HOSTNAME"]
+            if self.hostname not in sections:
+                raise ValueError("hostname %s is not a section in the configuration file %s" % (self.hostname, self.__ini_filename))
+            my_section = self.config[self.hostname]
+            self.__ip_command = my_section['ip_command']
+        return
+
 
     @property  # Invoke this method when setting the name of the ip command.  On some machines, it is /sbin/ip
     # and on others it is /usr/sbin/ip or /usr/bin/ip
@@ -49,14 +51,51 @@ class FixedConfiguration(object):
 
 
 if __name__ == "__main__":
+
+    from routing import routing
+
+
+    def make_test_nbmdt_ini_file():
+        """Make a test ini file """
+        routing = routing.Routes()
+        default_gateway = routing.default_gateway()
+        contents="""
+[jeffs-laptop]
+ping_targets: f5.com, commercialventvac.com, google.com
+ping_command: /bin/ping
+ip_command: /sbin/ip
+default_gateway: 192.168.0.1
+
+[jeffs-desktop]
+ping_targets: f5.com, commercialventvac.com, google.com
+ping_command: /bin/ping
+ip_command: /sbin/ip
+default_gateway: 192.168.8.1        
+
+[jeffs_house]
+ping_targets: f5.com, commercialventvac.com, google.com
+ping_command: /bin/ping
+ip_command: /sbin/ip
+default_gateway: 192.168.0.1
+
+[bad]
+ping_targets:
+        
+
+        """
+        with open("nbmdt.ini", mode="wt") as f:
+            f.write(contents)
+        return
+
+# Main test program
     try:
         fixed_configuration = FixedConfiguration("XyZZy.txt")
     except FileNotFoundError as f:
         print("Raise FileNotFound error as expected", file=sys.stderr)
-    else:
-        print("The FixedConfiguration.__init__ method did *not* raise the FileNotFound exception as it should.")
+
+    make_test_nbmdt_ini_file()
+
     fixed_configuration = FixedConfiguration("nbmdt.ini")
-    ip_command = fixed_configuration.ip_command
 
     try:
         fixed_configuration.ip_command="This should fail: calling the set_ip_command method"
@@ -64,5 +103,14 @@ if __name__ == "__main__":
         print("fixed_configuration.set.ip_command failed as expected with NotImplementedError")
     else:
         print("EPIC FAIL!!! fixed_configuration.set_ip_command worked! Should have raised NotImplementedError")
+    ip_command = fixed_configuration.ip_command
+    for section in fixed_configuration.sections():
 
-    print(fixed_configuration.ip_command, fixed_configuration.hostname)
+        print(fixed_configuration[section].ip_command )
+        ping_target_str = fixed_configuration[section]['ping_targets']
+        ping_targets_list = ",".split(ping_target_str)
+        for ping_target in ping_targets_list:
+            print(f"In section {section} ping target is {ping}")
+
+
+
