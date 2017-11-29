bb3e3168c3765d8f689062211a955b6d2482bc64
Jeffrey Silverman <jsilverman@gmail.com>
2017-11-29 05:10:39 -0800

    Merging from development to blue-sky

--- nbmdt.py
+++ nbmdt.py
@@ -10,12 +10,13 @@
 import subprocess
 import socket
 import collections
+import interfaces
 import routes
 from routes import IPv4Route as IPv4_route
 from routes import IPv6Route as IPv6_route
-import interfaces
+import networks
 import yaml
-
+import termcolor
 
 from enum import Enum
 class Modes(Enum):
@@ -25,32 +26,27 @@ class Modes(Enum):
     TEST=4
 
 class ErrorLevels(Enum):
-    OKAY=1         # Everything is working properly
-    DEGRADED=2     # Many things are working properly
-    CHANGED=3      # Things are working but they aren't what's in the database
-    UNKNOWN=4      # The program can't tell if something is working or not
-    DOWN=5         # It's completely not working
+    LABEL=0
+    NORMAL=1          # The resource is working properly in all respects
+    DEGRADED=2        # The resource is working properly but there are resources it depends on
+                      # that are down.  Also, it is slow
+    CHANGED=3         # The resource is working, but it doesn't agree with the .ini
+                      # file.
+    DOWN_ACKED=4      # The resource down problem has been ackowledged
+    DOWN_DEPENDENCY=5 # The resource is down because something it depends on is down
 
-import termcolor
-    colors={}
-    colors[OKAY] = termcolor.COLORS.mpl
+    DOWN_UNKNOWN=6    # It's completely not working for unknown reasons
 
-class Networks(object):
-    def __init__(self):
-        self.remote_hosts = []
+    colors={LABEL: 'grey', NORMAL: 'green', DEGRADED:'yellow', CHANGED:'blue',
+            DOWN_ACKED: 'cyan', DOWN_DEPENDENCY: 'magenta', DOWN_UNKNOWN: 'red' }
 
-    @staticmethod
-    def find_networks():
-        pass
+    if __name__ == '__main__':
+        for color in [LABEL, NORMAL, DEGRADED, CHANGED, DOWN_ACKED, DOWN_DEPENDENCY, DOWN_UNKNOWN]:
+            termcolor.cprint(colors[color],colors[color] )
+        termcolor.cprint('BLINKING red', colors[DOWN_UNKNOWN], attrs=['reverse'] )
 
 
-class Applications(object):
-    def __init__(self):
-        self.applications = []
 
-    @staticmethod
-    def find_applications():
-        pass
 
 
 class SystemDescription(object):
@@ -68,8 +64,8 @@ class SystemDescription(object):
             self.link_db = interfaces.PhysicalInterface.get_all_physical_interfaces()
             # Create a dictionary, keyed by link name, of the logical interfaces, that is, interfaces with addresses
             self.addr_db = interfaces.LogicalInterface.get_all_logical_interfaces()
-            self.routes_4_db = routes.Routes.get_all_ipv4_routes()
-            self.routes_6_db = routes.Routes.get_all_ipv4_routes()
+            self.routes_4_db = routes.IPv4Route.find_ipv4_routes()
+            self.routes_6_db = routes.IPv6Route.find_ipv6_routes()
 
             #        self.ipv4_routes = addresses.Ipv4Routes()
     #        self.ipv6_routes = addresses.Ipv6Routes()
@@ -82,9 +78,11 @@ class SystemDescription(object):
     #        self.name = name
         else:
             y = yaml.safe_load(configuration_file)
-            self.interfaces = y['interfaces']
-            for interface in self.interfaces:
-                self.ipv4_address = interfaces
+            self.link_db = y['interfaces']
+            self.addr_db = dict()
+            for link in self.link_db:
+                self.addr_db[link] = y['interface']['ipv4_addr']
+
 
 
     @staticmethod
@@ -128,14 +126,40 @@ class SystemDescription(object):
 
 
 
+
 if __name__ == "__main__" :
+    def boot (self, nominal : SystemDescription, current : SystemDescription ):
+        """This method tests the system at boot time.
+        """
+        results = ErrorLevels.NORMAL
+        for interface in current.link_db:
+            if interface not in nominal.link_db :
+                results = ErrorLevels.CHANGED
+            interfaces_status = interface.interfaces_status( )
+            if interfaces_status != True :
+                results = ErrorLevels.DOWN_UNKNOWN
+        for interface in nominal.link_db:
+            if interface not in current.link_db:
+                # if interface is in current.link_db, then it has already been
+                # testerd.
+                if ErrorLevels.CHANGED > results :
+                    results = ErrorLevels.CHANGED
+                interfaces_status = interface.interfaces_status( )
+                if interfaces_status != True :
+                    results = ErrorLevels.DOWN_UNKNOWN
+        return results
+
+
+
+
     nominal_system_description = SystemDescription ( configuration_file="nominal.txt" )
     current_system_description = SystemDescription ( )
 
-    mode = Modes.TEST   # This will be an option to the program some day.
+    mode = Modes.BOOT   # This will be an option to the program some day.
 
-    if mode == Modes.TEST :
-        test ( nominal_system_description, current_system_description )
+    if mode == Modes.BOOT :
+         results = boot ( nominal_system_description, current_system_description )
+         print()
 
 
 
