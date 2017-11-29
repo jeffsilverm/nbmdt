fe91373750a48c569ae70b657de345c8036ba4c0
Jeffrey Silverman <jsilverman@gmail.com>
2017-11-29 05:05:03 -0800

    A stub for applications

---
+++ applications.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# The Network Boot Monitor Diagnostic tool
+#
+# This file contains information on networks
+
+
+
+class Applications(object):
+    def __init__(self):
+        self.applications = []
+
+    @staticmethod
+    def find_applications():
+        pass
