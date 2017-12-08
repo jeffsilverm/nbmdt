3217854e63e55e6058273e6fbe5b2275d8fffd62
Jeff Silverman <jeffsilverm@gmail.com>
2017-12-07 20:42:10 -0800

    Working on DNS testing.

--- tests.py
+++ tests.py
@@ -21,10 +21,12 @@ import subprocess
 import re
 # Get dns from https://github.com/rthalley/dnspython
 import dns.resolver
+import sys
+import socket       # Required for testing the Tests.dns method
 
 PING_COMMAND = "/bin/ping"      # for now
 
-class Tests():
+class Tests(object):
 
     def __init__(self):
         self.my_resolver = dns.resolver.Resolver()
@@ -86,21 +88,61 @@ class Tests():
 
         return ( down, slow )
 
-    def dns (self,  remote_host:str, dns_server:str=None ):     # type annotation should be str or list
+    def dns (self,  remote_host:str, dns_server:str=None):     # type annotation should be str or list
         """This method tests that the name server DNS server can return queries and that it gets the right address for
         remote_host
-        :param  dns_server  The name or IP address of the DNS server to test
+        :param  dns_server  The name or IP address of the DNS server to test.  This might be a single name server, in
+                            which case, it will be a string.  This might be a list of name servers, in which case, it will
+                            be a list of strings
         :param  remote_host     The name of the remote host to test.
         A future version might pass the "correct" answer
         """
 
-        answer.response.answer[0]
 
         if dns_server is None:
+            # This is not the right way to do it.  The right way to do it would be to query /etc/resolv.conf and get
+            # the list of nameservers.  Also, this only works for IPv4.
             self.my_resolver.nameservers = ['8.8.8.8']
         elif isinstance( dns_server, list):
             self.my_resolver.nameservers = dns_server
         else:
             self.my_resolver.nameservers = [dns_server]
 
-answer = my_resolver.query('google.com')
+        answer = self.my_resolver.query(remote_host)
+        print( answer.response.answer[0], file=sys.stderr)
+        return answer.response.answer[0]
+
+
+if __name__ == "__main__":
+    def test_dns ( remote_host, dns_server=None ):
+        """This method tests the dns method by comparing what it returns against what gethostbyname returns.
+        Returns true if correct"""
+        correct = socket.gethostbyname(remote_host)
+        tests = Tests()
+        if dns_server is None:
+            test_answer = tests.dns(remote_host, dns_server)
+            my_answer = test_answer.items.O.address
+            print(f"'Correct' answer: {correct}.  My answer {my_answer}", file=sys.stderr)
+            return my_answer == test_answer
+        else:
+            with open("/etc/resolv.conf","r") as f:
+                configuration=f.readlines()
+            ns_list=[]
+            for l in configuration:
+                if "nameserver" in l:
+                    ns_list.append(l.split()[1])
+            for dns_server in ns_list:
+                try:
+                    test_answer = tests.dns(remote_host, dns_server)
+                except Exception as e:
+                    print(f"tests.dns raised the {str(e)} exception on nameserver {dns_server}  Retrying", file=sys.stderr)
+                    continue
+            return correct == test_answer
+
+    for remote_host in ['jeffsilverman.com', 'f1.com', 'commercialventvac.com']:
+        for nameserver in [None, "8.8.8.8", "2001:4860:4860::8888", "2001:4860:4860::8884"  ]:
+            assert test_dns(remote_host, nameserver ), f"{remote_host} failed with nameserver {'default' if nameserver is None else nameserver} "
+
+
+
+
