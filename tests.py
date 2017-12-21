e74af5bb3fd1a6c21a42343c65b69cd14df3a179
Jeffrey Silverman <jsilverman@gmail.com>
2017-12-20 23:46:50 -0800

    Resolve Issue 5, removed the ping4 and ping6 methods. Still to come: Issue 7, move the dns method to a new DNS class in another file

--- tests.py
+++ tests.py
@@ -25,91 +25,13 @@ import sys
 import socket       # Required for testing the Tests.dns method
 from termcolor import colored
 
-PING_COMMAND = "/bin/ping"      # for now
+
 
 class Tests(object):
 
     def __init__(self):
         self.my_resolver = dns.resolver.Resolver()
 
-
-    @classmethod
-    def ping4(cls, remote_ipv4:str, count:str="10", min_for_good:int=8, slow_ms:float=100.0 ):
-
-        """This does a ping test of the machine remote_ipv4.
-        :param  remote_ipv4     the remote machine to ping
-        :param  min_for_good     The minimum number of successful pings required for the machine to be up
-        :param  count           number of packets to be sent, default is 10
-        :param  min_for_good    the number of packets that must be returned in order to consider the remote machine "up"
-        :param  slow_ms         The maximum amount of time, in milliseconds, that is allowed to transpire before the
-                                remote machine will be considered "slow"
-
-        """
-
-        SLOW_MS = 100.0  # milliseconds.  This should be a configuration file option
-        cpi = subprocess.run(args=[PING_COMMAND, '-n', count, remote_ipv4 ],
-                             stdin=None,
-                             input=None,
-                             stdout=subprocess.PIPE, stderr=None,
-                             shell=False, timeout=None,
-                             check=False, encoding="utf-8", errors=None)
-        if cpi.returncode != 0:
-            raise subprocess.CalledProcessError
-        # Because subprocess.run was called with encoding=utf-8, output will be a string
-        results = cpi.stdout
-        lines = results.split('\n')[:-1]  # Don't use the last element, which is empty
-
-        """
-        jeffs@jeffs-laptop:~/nbmdt (development)*$ ping -c 4  f5.com
-        PING f5.com (104.219.110.168) 56(84) bytes of data.
-        64 bytes from 104.219.110.168 (104.219.110.168): icmp_seq=1 ttl=249 time=24.0 ms
-        64 bytes from 104.219.110.168 (104.219.110.168): icmp_seq=2 ttl=249 time=23.8 ms
-        64 bytes from 104.219.110.168 (104.219.110.168): icmp_seq=3 ttl=249 time=23.3 ms
-        64 bytes from 104.219.110.168 (104.219.110.168): icmp_seq=4 ttl=249 time=46.3 ms
-        
-        --- f5.com ping statistics ---
-        4 packets transmitted, 4 received, 0% packet loss, time 3003ms
-        rtt min/avg/max/mdev = 23.326/29.399/46.300/9.762 ms
-        jeffs@jeffs-laptop:~/nbmdt (development)*$ 
-        
-        """
-        # Failsafe initialization
-        slow = True
-        down = True
-        # loop through output of ping command
-        for line in lines:
-            if "transmitted " in line:
-                # re.findall returns a list of length 1 because there is 1 match to the RE
-                packet_counters = re.findall("(\d+).*?(\d+).*received.*(\d+).*()", line)[0]
-                packets_xmit=packet_counters[0]
-                packets_rcvd=packet_counters[1]
-                down = int(packets_rcvd) < min_for_good
-            elif "rtt " == line[0:3]:         # crude, I will do something better, later
-                # >>> re.findall("\d+\.\d+", "rtt min/avg/max/mdev = 23.326/29.399/46.300/9.762 ms")
-                # ['23.326', '29.399', '46.300', '9.762']
-                # >>>
-                # The RE matches a fixed point number, and there are 4 of them.  The second one is the average
-                numbers = re.findall("\d+\.\d+", line)
-                slow = int(numbers[1]) > SLOW_MS
-            else:
-                pass
-
-        return ( down, slow )
-
-    @classmethod
-    def ping6(cls, remote_ipv6:str, count:int=10, min_for_good:int=8, slow_ms:float=100.0 ):
-
-        """This does a ping test of the machine remote_ipv6.
-        :param  remote_ipv6     the remote machine to ping
-        :param  min_for_good     The minimum number of successful pings required for the machine to be up
-        :param  count           number of packets to be sent, default is 10
-        :param  min_for_good    the number of packets that must be returned in order to consider the remote machine "up"
-        :param  slow            The maximum amount of time, in milliseconds, that is allowed to transpire before the
-                                remote machine will be considered "slow"
-
-        """
-        colored.cprint("ping6 isn't implemented yet", "yellow")
-        return True
 
 
         # type annotation should be str or list
@@ -161,6 +83,7 @@ if __name__ == "__main__":
                     test_answer = tests.dns(remote_host, dns_server)
                 except Exception as e:
                     print(f"tests.dns raised the {str(e)} exception on nameserver {dns_server}  Retrying", file=sys.stderr)
+                    test_answer = None
                     continue
             return correct == test_answer
 
