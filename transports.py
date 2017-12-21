11e517f74c6fdc81987c32554bc477720b8d47e7
Jeffrey Silverman <jsilverman@gmail.com>
2017-12-20 23:23:23 -0800

    Resolve Issue 5, renamed IPv4_address to IPv4Address and IPv6_address to IPv6Address

--- transports.py
+++ transports.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from enum import Enum
-from routes import IPv4_address as ipv4
-from routes import IPv6_address as ipv6
+# Issue 5 renamed IPv4_address to IPv4Address and IPv6_address to IPv6Address
+from routes import IPv4Address as ipv4
+from routes import IPv6Address as ipv6
 from socket import AF_INET, AF_INET6
 
 
