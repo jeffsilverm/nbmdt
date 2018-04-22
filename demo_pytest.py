e15b011d2eddc292d4e68efbd559cbf269d170d5
Jeffrey Silverman <jsilverman@gmail.com>
2018-04-21 17:30:33 -0700

    Started working on demo_pytest.py

---
+++ demo_pytest.py
@@ -0,0 +1,25 @@
+import pytest
+
+import interface
+
+# See https://gist.githubusercontent.com/alysivji/a2535eae67a5569fa0169766c228f818/raw/d166e13401f6a2eb01719074dd7b80a67bfcc300/test_interface_pytest.py
+# and https://gist.github.com/alysivji/a2535eae67a5569fa0169766c228f818
+# and https://disqus.com/home/discussion/siv-scripts/adding_function_arguments_to_pytest_fixtures/?utm_source=reply&utm_medium=email&utm_content=read_more#comment-3865338855
+
+
+@pytest.fixture(scope='session')
+def run_command_mock(mocker):
+    def _create_run_command_mock(mock_return_value):
+        mock_run = mocker.MagicMock(name='run command mock')
+        mock_run.return_value = "foo"
+        run_command_patch = mocker.patch('interface.run_command', new=mock_run)
+        return run_command_patch
+
+    return _create_run_command_mock
+
+
+def test_interface_run_command(ifname : str) -> boolean :
+
+    en01_obj = interface.Interface(ifname)
+    assert isinstance(eno1_obj.mtu, "str"), f"mtu should be a string"
+    assert en01_obj.mtu == "1500", f"The MTU should be 1500 but is really {eno1_obj}"
