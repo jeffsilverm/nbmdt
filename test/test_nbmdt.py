4382c16395f418f2703e78ca5260e8ea07b84bac
Jeffrey Silverman <jsilverman@gmail.com>
2018-07-31 01:52:43 -0700

    Tests some, but not all, of the mode options and the --debug option.

--- test/test_nbmdt.py
+++ test/test_nbmdt.py
@@ -9,6 +9,7 @@ from typing import List
 import subprocess
 import pytest
 import nbmdt
+import argparse
 
 assert sys.version_info.major >= 3 and sys.version_info.minor >= 6, "Python must be later than version 3.6, " \
                                                                     f"is currently {str(sys.version_info)} "
@@ -25,54 +26,51 @@ def test_argparse():
         """
         sys.argv = ["nbmdt.py"] + args_list
         with pytest.raises(ValueError):
-            parsed_args = nbmdt.arg_parser()
+            (parsed_args, mode) = nbmdt.arg_parser()
             assert parsed_args is None, f"There was a deliberate args parse error, this code comes after the " \
                                         f"exception was supposed to be raised. parsed_args should " \
                                         f"be None but is {str(parsed_args)} " \
-                                 f"args_list is {str(args_list)}"
+                                 f"args_list is {str(options_list)}"
 
     def test_boot_mode(option):
         # Test that the boot mode works
 
         sys.argv = ["nbmdt.py", option]
         try:
-            args = nbmdt.arg_parser()
+            (options, mode) = nbmdt.arg_parser()
         except ValueError as v:
             print(f"nbmdt.main raised a ValueError exception. option is {option}")
-        assert isinstance(args, tuple), f"The return from nbmdt.arg_parser should be a tuple but is {str(type(args))}"
-        assert len(args)==2, f"The return from nbmdt.arg_parser should be a length 2 but is {len(args)}"
-        assert args.boot, "args.boot is False when it should be True"
-        assert not args.monitor, "the --monitor switch is True when it should be False"
-        assert not args.diagnose, "the --diagnose switch is True when it should be False"
+        assert isinstance(options, argparse.Namespace), f"The return from nbmdt.arg_parser should be a 'argparse.Namespace' but is {str(type(options))}"
+        assert options.boot, "options.boot is False when it should be True"
+        assert not options.monitor, "the --monitor switch is True when it should be False"
+        assert not options.diagnose, "the --diagnose switch is True when it should be False"
 
     def test_monitor_mode(option):
         sys.argv = ["nbmdt.py", option]
-        args = nbmdt.arg_parser()
-        assert isinstance(args, tuple), f"The return from nbmdt.arg_parser should be a tuple but is {str(type(args))}"
-        assert len(args)==2, f"The return from nbmdt.arg_parser should be a length 2 but is {len(args)}"
-        assert not args.boot, "args.boot is True when it should be False"
-        assert args.monitor, "args.monitor is False when it should be True"
-        assert not args.diagnose, "args.diagnose switch is False when it should be True"
+        (options, mode) = nbmdt.arg_parser()
+        assert isinstance(options, argparse.Namespace), f"The return from nbmdt.arg_parser should be a 'argparse.Namespace' but is {str(type(options))}"
+        assert not options.boot, "options.boot is True when it should be False"
+        assert options.monitor, "options.monitor is False when it should be True"
+        assert not options.diagnose, "options.diagnose switch is False when it should be True"
 
     def test_diagnose_mode(option):
         sys.argv = ["nbmdt.py", option]
-        args = nbmdt.arg_parser()
-        assert isinstance(args, tuple), f"The return from nbmdt.arg_parser should be a tuple but is {str(type(args))}"
-        assert len(args)==2, f"The return from nbmdt.arg_parser should be a length 2 but is {len(args)}"
-        assert not args.boot, "args.boot is True when it should be False"
-        assert not args.monitor, "args.monitor is True when it should be False"
-        assert args.diagnose, "args.diagnose is False when it should be True"
+        (options, mode) = nbmdt.arg_parser()
+        assert isinstance(options, argparse.Namespace), f"The return from nbmdt.arg_parser should be a 'argparse.Namespace' but is {str(type(options))}"
+        assert not options.boot, "options.boot is True when it should be False"
+        assert not options.monitor, "options.monitor is True when it should be False"
+        assert options.diagnose, "options.diagnose is False when it should be True"
 
     def test_debug_option():
         # Does the --debug option work?
         sys.argv = ["nbmdt.py", "--diagnose", "--debug"]
-        args = nbmdt.arg_parser()
-        assert isinstance(args.debug, bool), f"args.debug should be boolean but is actually {type(args.debug)}"
-        assert args.debug, "debug switch was given, but args.debug is False"
+        (options, mode) = nbmdt.arg_parser()
+        assert isinstance(options.debug, bool), f"options.debug should be boolean but is actually {type(options.debug)}"
+        assert options.debug, "debug switch was given, but options.debug is False"
         sys.argv = ["nbmdt.py", "--diagnose"]
-        args = nbmdt.arg_parser()
-        assert isinstance(args.debug, bool), f"args.debug should be boolean but is actually {type(args.debug)}"
-        assert not args.debug, "debug switch was not given, but args.debug is True"
+        (options, mode) = nbmdt.arg_parser()
+        assert isinstance(options.debug, bool), f"options.debug should be boolean but is actually {type(options.debug)}"
+        assert not options.debug, "debug switch was not given, but options.debug is True"
         # The first opportunity to actually test nbmdt.py as a program
         stdout_str, stderr_str = run_nbmdt("--debug", "--diagnose")
         assert "debug" in stderr_str.lower(), "while running nbmdt.py, the --debug option was given but debug did not appear in stderr"
