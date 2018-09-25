297f84d7bfa1b0200754ca330d44f2ae5fa8c367
Jeffrey Silverman <jsilverman@gmail.com>
2018-09-25 16:17:53 -0700

    A fast test of the nbmdt

---
+++ smoke_test.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# A smoke test of the nbmdt.py program
+success=0
+if ! python3 nbmdt.py --boot; then success=1; fi
+if ! python3 nbmdt.py --monitor 20080; then success=1; fi
+if ! python3 nbmdt.py --diagnose xyzzy.json; then success=1; fi
+if ! python3 nbmdt.py --test ethernet=eno1; then success=1; fi
+if [ $success -eq 0 ] ; then echo "smoke test PASSES"; else echo "smoke test **FAILS******"; fi
+exit $success
+
+
