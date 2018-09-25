f245d45c05fb0427541988f43a928502dfdd06a9
Jeffrey Silverman <jsilverman@gmail.com>
2018-09-24 22:52:01 -0700

    Passes smoke test

--- session.py
+++ session.py
@@ -6,8 +6,8 @@ from constants import ErrorLevels
 class Session(object):
 
     def __init__(self, session_str):
-        self.session_str = session_str
-        self.layer = Layer()
+        self.session_str = "SESSION"
+        self.layer = Layer(name=session_str)
 
     def get_status(self) -> ErrorLevels :
         return self.layer.get_status()
@@ -16,4 +16,7 @@ class Session(object):
     def discover(cls):
         s = cls.__init__(self=cls, session_str="A session string, just a placeholder")
         return s
+
+if "__main__" == __name__ :
+    s = Session("test session")
 
