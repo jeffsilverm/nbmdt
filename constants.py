ff426041dc33dc5284dfaae38112826d299ad70e
Jeffrey Silverman <jsilverman@gmail.com>
2019-03-02 01:58:39 -0800

    Issue 29 and also some reformatting

--- constants.py
+++ constants.py
@@ -2,17 +2,15 @@
 #
 # This file contains constants for the NBMDT
 #
-from enum import IntEnum, Enum, auto
-import sys
 # Issue 29 https://github.com/jeffsilverm/nbmdt/issues/29
 import typing
-from typing import Tuple, List
+from enum import IntEnum, Enum, auto
 
-port = 8722  # Default port that the server listens on.  Provides a RESTful interface
+port = 8722  # Default port that the server listens on.  Provides a RESTful
+# interface
 
 
 class Modes(IntEnum):
-
     BOOT = 1
     MONITOR = 2
     DIAGNOSE = 3
@@ -21,7 +19,6 @@ class Modes(IntEnum):
 
 
 class OperatingSystems(IntEnum):
-
     LINUX = 1
     MAC_OS_X = 2
     WINDOWS = 3
@@ -58,9 +55,20 @@ class OSILevels(Enum):
     APPLICATION = 7
 
 
+class Protocols(Enum):
+    # These values are from /etc/protocols
+    IP = 0  # IPv4.
+    TCP = 6
+    UDP = 17
+    IPv6 = 41  # is ipv6 in /etc/protocols
+    IPv6_route = 43  # is ipv6-route in /etc/protocols
+    IPv6_frag = 44  # is ipv6-frag in /etc/protocols
+
+
 class Layers(IntEnum):
     # This is from the nbmdt_user_manual
-    #  ethernet, wifi, ipv4, ipv6, neighbors, dhcp4, dhcp6, router, nameserver,  local_ports, isp_routing, remote_ports.
+    #  ethernet, wifi, ipv4, ipv6, neighbors, dhcp4, dhcp6, router,
+    # nameserver,  local_ports, isp_routing, remote_ports.
     ETHERNET = 1
     WIFI = 2
     IPV4 = 3
@@ -97,23 +105,32 @@ class ErrorLevels(IntEnum):
         Normal
     All is well.  The resource is working properly in all respects
         Slow
-    The monitor is measuring traffic flow through the resource or the delay required to reach the interface, and it is
+    The monitor is measuring traffic flow through the resource or the delay
+    required to reach the interface, and it is
     outside acceptable limits.  The interface is still working.
         Degraded due to an upstream dependency failure.
-    The resource is at increased risk of failure because a dependency is down.  However, the dependency has some
-    redundancy so the fact that there is an upstream failure does not mean that this interface is down.  For example,
-    there are multiple DNS servers.  If a single server fails, the resolver will pick a different name server.  So DNS
-    will work, but there will be fewer servers than normal and a greater risk of subsequent failure.
+    The resource is at increased risk of failure because a dependency is
+    down.  However, the dependency has some
+    redundancy so the fact that there is an upstream failure does not mean
+    that this interface is down.  For example,
+    there are multiple DNS servers.  If a single server fails, the resolver
+    will pick a different name server.  So DNS
+    will work, but there will be fewer servers than normal and a greater risk
+    of subsequent failure.
         Down cause unknown
     The resource is flunking a health check for some reason.
         Down due to a missing or failed dependency
-    The resource is flunking a health check or is down because something it depends on is down.
+    The resource is flunking a health check or is down because something it
+    depends on is down.
         Down Acknowledged
-    The resource is down and an operator has acknowledged that the interface is down.  This will only happen while
+    The resource is down and an operator has acknowledged that the interface
+    is down.  This will only happen while
     monitoring, never at boot time or while diagnosing or designing
         Changed
-    The resource is in the configuration file, but the monitor cannot find it, or the auto configuration methods found
-    a resource that should be in the configuration file, but isn't.  Classic example is a NIC changing its MAC address.
+    The resource is in the configuration file, but the monitor cannot find
+    it, or the auto configuration methods found
+    a resource that should be in the configuration file, but isn't.  Classic
+    example is a NIC changing its MAC address.
         Other problem
     Something else is wrong that doesn't fit into the above categories.
 
@@ -121,36 +138,50 @@ class ErrorLevels(IntEnum):
 
     NORMAL = 1  # Everything is working properly
     SLOW = 2  # Up, but running slower than allowed
-    DEGRADED = 3  # Up, but something that this thing partly depends on is down (e.g. 1 DNS server or 1 NTP
+    DEGRADED = 3  # Up, but something that this thing partly depends on is
+    # down (e.g. 1 DNS server or 1 NTP
     #  server)
     DOWN = 4  # It flunks the test, cause unknown
-    DOWN_DEPENDENCY = 5  # It flunks the test, but it is known to be due to a dependency
-    DOWN_ACKNOWLEDGED = 6  # It flunks the test, but somebody has acknowledged the problem
+    DOWN_DEPENDENCY = 5  # It flunks the test, but it is known to be due to a
+    #  dependency
+    DOWN_ACKNOWLEDGED = 6  # It flunks the test, but somebody has
+    # acknowledged the problem
     CHANGED = 7  # The resource works, but something about it has changed
     OTHER = 8  # A problem that doesn't fit into any of the above categories
-    UNKNOWN = 9  # We genuinely do not know the status of the entity, either because the test has not been
+    UNKNOWN = 9  # We genuinely do not know the status of the entity, either
+    # because the test has not been
     # run yet or conditions are such that the test cannot be run correctly.
 
 
-# If termcolor isn't good enough (it has only 8 colors), try colored (which has 256),
-# https://pypi.python.org/pypi/colored/1.3.3.  Do not confuse the colored package with
+# If termcolor isn't good enough (it has only 8 colors), try colored (which
+# has 256),
+# https://pypi.python.org/pypi/colored/1.3.3.  Do not confuse the colored
+# package with
 # termcolor.colored
-colors = {ErrorLevels.NORMAL: ['black', 'on_green'], ErrorLevels.SLOW: ['black', 'on_yellow'],
-          ErrorLevels.DEGRADED: ['black', 'on_magenta'], ErrorLevels.DOWN: ['black', 'on_red'],
-          ErrorLevels.DOWN_DEPENDENCY: ['black', 'on_cyan'], ErrorLevels.DOWN_ACKNOWLEDGED: ['black', 'on_magenta'],
-          ErrorLevels.CHANGED: ['white', 'on_black'], ErrorLevels.OTHER: ['black', 'on_grey']}
+colors = {ErrorLevels.NORMAL: ['black', 'on_green'],
+          ErrorLevels.SLOW: ['black', 'on_yellow'],
+          ErrorLevels.DEGRADED: ['black', 'on_magenta'],
+          ErrorLevels.DOWN: ['black', 'on_red'],
+          ErrorLevels.DOWN_DEPENDENCY: ['black', 'on_cyan'],
+          ErrorLevels.DOWN_ACKNOWLEDGED: ['black', 'on_magenta'],
+          ErrorLevels.CHANGED: ['white', 'on_black'],
+          ErrorLevels.OTHER: ['black', 'on_grey']}
 # on is down
 # it as
-MAXINT=4294967296
-LAYERS_LIST = "ethernet,wifi,ipv4,ipv6,neighbors,dhcp4,dhcp6,router,nameserver,local_ports,isp_routing," \
-              "remote_ports,application, presentation,session,transport,network,datalink,physical"
+MAXINT = 4294967296
+LAYERS_LIST = "ethernet,wifi,ipv4,ipv6,neighbors,dhcp4,dhcp6,router," \
+              "nameserver,local_ports,isp_routing," \
+              "remote_ports,application, presentation,session,transport," \
+              "network,datalink,physical"
 
-# Moved here from nbmdt.py Issue 29 https://github.com/jeffsilverm/nbmdt/issues/29
+# Moved here from nbmdt.py Issue 29
+# https://github.com/jeffsilverm/nbmdt/issues/29
 type_application_dict = typing.Dict[str, "application.Application"]
 type_presentation_dict = typing.Dict[str, "presentation.Presentation"]
 type_session_dict = typing.Dict[str, "session.Session"]
 type_transport_dict = typing.Dict[str, "transport.Transport"]
 type_network_dict = typing.Dict[str, "network.Network"]
 type_datalink_dict = typing.Dict[str, "datalink.DataLink"]
-type_interface_dict = typing.Dict[str, "interface.Interface"]   # Issue 25 re-written
+type_interface_dict = typing.Dict[
+    str, "interface.Interface"]  # Issue 25 re-written
 type_physical_dict = typing.Dict[str, "physical.Physical"]  # Issue 29
