5a92079037f94099711c40769b1d0a17775f611b
Jeffrey Silverman <jeffsilverm@gmail.com>
2020-01-16 00:46:20 -0800

    Started writing a mock for interfaces.py

---
+++ test_interfaces.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# test class interfaces.PhysicalInterace
+
+from interfaces import PhysicalInterface
+from unittest.mock import patch
+from unittest import mock
+
+
+
+
+def test_interfaces():
+
+    mocker_interface = PhysicalInterface("eno1")
+    with patch("mock") as
+    mocker = mock.Mock()
+
+    if_eno1_obj = PhysicalInterface("eno1")
+
