67c18991808317122dd34693806735e20114e0a0
Jeff Silverman <jeffsilverm@gmail.com>
2024-10-28 10:11:36 -0700

    Added some variables to give a better sense of the magnitude of the problem

--- automated_network_monitor.sh
+++ automated_network_monitor.sh
@@ -7,7 +7,9 @@
 # Set to true if you want the script to use notify_send to send notifications
 USE_notify_send=true
 # How long to keep the notification visible in msec
-EXPIRE_TIME=60000
+END_EXPIRE_TIME=60000
+PART_EXPIRE_TIME=10000
+GOOD_EXPIRE_TIME=2000		# This is here only to reassure that the automated_network_monitor.sh script is running.
 #
 # How frequently to run (how long to sleep between runs) (10s = 10 seconds)
 SLEEP_TIME=10s
@@ -15,15 +17,16 @@ SLEEP_TIME=10s
 #
 # File to store the current state of the network
 TEMP_STATE_FILE=/tmp/network_monitor.txt
+#
 
 handle_error() {
   COMMAND=$1
   ARGUMENTS=$2
   RETURN=$3
   echo "The arguments were $@"
-  error_string="$(date -Is) THERE IS A PROBLEM $COMMAND $ARGUMENTS $RETURN"
+  error_string="$(date -Is) PROBLEM: $COMMAND $ARGUMENTS $RETURN"
   if $USE_notify_send; then
-    notify-send -u critical -t $EXPIRE_TIME  "$error_string"
+    notify-send -u critical -t $PART_EXPIRE_TIME  "$error_string"
   fi
 }
 
@@ -31,18 +34,35 @@ run_test() {
   COMMAND=$1
   shift
   ARGUMENTS=$@
-  if ! $COMMAND $ARGUMENTS > /dev/null 2>>/dev/null; then
+  if $COMMAND $ARGUMENTS > /dev/null 2>>/dev/null; then
+    ANY=true
+    NONE=false
+  else
+    EVERY=false
     RESULT=$?
     handle_error $COMMAND $ARGUMENTS $RESULT
   fi
 }
 
-while true; do  
+while true; do
+  EVERY=true	# if any test fails, then make this false
+  ANY=false	# if any test passes, then make this true
+  NONE=true	# if any test passes, then make this false.
   run_test http http://google.com
   run_test http https://amazon.com
   run_test ssh -4 -T git@gitlab.com 
   run_test ssh -6 -T git@gitlab.com
-  notify-send -u low -t 10000 "$(date -Is) ANM OK"
+  if $NONE; then
+     notify-send -u critical -t $END_EXPIRE_TIME "$(date -Is) ***** CATASTROPHIC FAILURE ******"
+  else
+# If NONE is true, then neither EVERY nor ANY may be true.
+    if $EVERY; then
+# Note that if EVERY is true then ANY will also be true.  But ANY could be true, even if EVERY was false, but only if NONE was also false
+      notify-send -u low -t $GOOD_EXPIRE_TIME "$(date -Is) ANM OK"
+    else 
+      notify-send -u normal -t $END_EXPIRE_TIME "$(date -Is) * There are some problems *"
+    fi
+  fi  
   sleep $SLEEP_TIME
 done
 
